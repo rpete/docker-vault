48a57beed3e6e7afdb707c420dd3bf2a7b43f7f9
Raymond Pete <rpete@broadinstitute.org>
2015-07-16 20:53:54 -0400

    fix spelling

--- run.sh
+++ run.sh
@@ -9,6 +9,6 @@ CONSUL_AGENT=`/sbin/ip route | awk '/default/ { print $3 }'`
 sed -i "s;%%VAULT_SERVER%%;${VAULT_SERVER};" "/etc/vault.config"
 sed -i "s;%%SERVER_CRT%%;${SERVER_CRT};" "/etc/vault.config"
 sed -i "s;%%SERVER_KEY%%;${SERVER_KEY};" "/etc/vault.config"
-sed -i "s;%%CONSUL_AGENT%%;${CONSUL_AGENT};" "/etc/vault.conf"
+sed -i "s;%%CONSUL_AGENT%%;${CONSUL_AGENT};" "/etc/vault.config"
 
 /usr/sbin/vault server -config=/etc/vault.config
