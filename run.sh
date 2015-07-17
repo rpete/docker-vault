a347451ab30daf8d16499bc18181c9ca115d8b4c
Raymond Pete <rpete@broadinstitute.org>
2015-07-16 20:44:38 -0400

    updates

--- run.sh
+++ run.sh
@@ -3,9 +3,12 @@
 VAULT_SERVER=${VAULT_SERVER:-https://localhost}
 SERVER_CRT=${SERVER_CRT:-server.crt}
 SERVER_KEY=${SERVER_KEY:-server.key}
+CONSUL_AGENT=`/sbin/ip route | awk '/default/ { print $3 }'`
+
 
 sed -i "s;%%VAULT_SERVER%%;${VAULT_SERVER};" "/etc/vault.config"
 sed -i "s;%%SERVER_CRT%%;${SERVER_CRT};" "/etc/vault.config"
 sed -i "s;%%SERVER_KEY%%;${SERVER_KEY};" "/etc/vault.config"
+sed -i "s;%%CONSUL_AGENT%%;${CONSUL_AGENT};" "/etc/vault.conf"
 
 /usr/sbin/vault server -config=/etc/vault.config
